f7800024c41ff4c7dad61254bc869ca718f030a5
Sei <josephdesilva5@gmail.com>
2026-02-07 09:11:37 +0800

    added Student and Course with getters, setters, display, private vars

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,18 @@ package org.example;
 
 public class Main{
     public static void main(String[] args) {
+        Student s = new Student();
+        Course c = new Course();
 
+        s.setStudentID("123");
+        s.setStudentName("Jusip");
+        s.setProgram("BSIT");
+
+        c.setCourseID("1001");
+        c.setCourseName("InteProg");
+        c.setProgram("Information Technology");
+
+        s.display();
+        c.display();
     }
 }
