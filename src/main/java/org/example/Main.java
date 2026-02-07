7c4a066fd63ef653fadae86d5204cd86cbdbc963
Sei <josephdesilva5@gmail.com>
2026-02-07 10:20:33 +0800

    some touches

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,17 +3,17 @@ package org.example;
 public class Main{
     public static void main(String[] args) {
         Student s = new Student();
-        Course c = new Course();
-
         s.setStudentID("123");
         s.setStudentName("Jusip");
         s.setProgram("BSIT");
 
+        Course c = new Course();
         c.setCourseID("1001");
         c.setCourseName("InteProg");
         c.setProgram("Information Technology");
 
         s.display();
         c.display();
+
     }
 }
